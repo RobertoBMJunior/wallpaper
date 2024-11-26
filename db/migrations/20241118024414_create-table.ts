952ae3409364265c0ddd38d60e9060779a2d5cd5
Roberto Junior <robertojuniorazul@gmail.com>
2024-11-25 23:54:26 -0300

    Criei a tabela images

--- db/migrations/20241118024414_create-table.ts
+++ db/migrations/20241118024414_create-table.ts
@@ -1,10 +1,15 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('images', function (table) {
+    table.uuid('id').primary() // Auto increment id
+    table.string('name').notNullable() // Nome da imagem
+    table.date('upload_date').notNullable() // Data do upload
+    table.text('tags') // Tags da imagem
+    table.string('image_path').notNullable() // Caminho ou URL da imagem
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('images')
 }
-
